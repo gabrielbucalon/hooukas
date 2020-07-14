08971e0dabffed616f1826557e48e825d5ad314d
gabriel.bucalon <bucalongabriell@gmail.com>
2020-07-14 12:19:13 -0300

    fix | remove position absolute on navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { User } from './pages/auth/model/User';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { Router } from '@angular/router';
+import { AuthService } from './pages/auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'hooukas';
+
+  currentUser: User;
+  private _subscription: Subscription
+
+  constructor(private _router: Router,
+    private _authenticationService: AuthService,) { }
+
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+  logout() {
+
+  }
 }
