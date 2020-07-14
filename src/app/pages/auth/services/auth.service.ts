61fd5d4f6215cbb3021e58449f455ef63efff865
gabriel.bucalon <bucalongabriell@gmail.com>
2020-07-14 10:28:34 -0300

    feature | create service guard, interceptor and adjust on auth service

--- src/app/pages/auth/services/auth.service.ts
+++ src/app/pages/auth/services/auth.service.ts
@@ -1,16 +1,44 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Address } from '@/pages/auth/model/User';
+import { Address, User } from '@/pages/auth/model/User';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
   getAddress(cep: string): Observable<Address> {
     return this.http.get<Address>(`https://viacep.com.br/ws/${cep}/json/`);
+  }
+
+
+  //////////////////////////////////////////////////////////////////////////
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http.post<any>(`/users/authenticate`, { username, password })
+      .pipe(map(user => {
+        user.authdata = window.btoa(username + ':' + password);
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.currentUserSubject.next(user);
+        return user;
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
   }
 }
