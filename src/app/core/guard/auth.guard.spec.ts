3de08902a7a44b6e715a2dff9e6ca34c6910aaec
gabriel.bucalon <bucalongabriell@gmail.com>
2020-07-14 10:32:25 -0300

    fix | rename files

--- src/app/core/guard/auth.guard.spec.ts
+++ src/app/core/guard/auth.guard.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GuardGuard } from './guard.guard';
+import { AuthGuard } from './guard.guard';
 
-describe('GuardGuard', () => {
-  let guard: GuardGuard;
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    guard = TestBed.inject(GuardGuard);
+    guard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
