134072be68446b01a0f907d30ebc37ad0d1fee70
gabriel.bucalon <bucalongabriell@gmail.com>
2020-07-17 20:08:46 -0300

    feature | create service product

---
+++ src/app/shared/services/products/products.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor() { }
+
+
+  addProduct(){
+
+  }
+
+  removeProduct(){
+
+  }
+
+  updateProduct(){
+
+  }
+
+  getAllProducts(){
+
+  }
+
+  getByIdProduct(){
+
+  }
+}
