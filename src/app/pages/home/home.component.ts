f2a56b73d7281c2fb3de2b492ee77fcab9a6fd2e
gabriel.bucalon <bucalongabriell@gmail.com>
2020-07-31 19:56:05 -0300

    feat and fix: change model product and get products

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateEditComponent } from '../products/create-edit/create-edit.component';
 import { ProductsService } from '@/shared/services/products/products.service';
+import { Products } from '@/shared/models/Products';
 
 
 @Component({
@@ -11,18 +12,21 @@ import { ProductsService } from '@/shared/services/products/products.service';
 })
 export class HomeComponent implements OnInit {
 
-  products: any;
+  products: Array<Products>;
   constructor(public dialog: MatDialog, private productsService: ProductsService) { }
 
   ngOnInit(): void {
     this.getProducts();
   }
 
-  getProducts(){
+ async getProducts(){
+    let product = Array<Products>();
     this.productsService.getAllProducts().subscribe((res: any) => {
-      res.docs.forEach(element => {
-        console.log(element.data());
+      res.docs.forEach(async (element) => {
+        product.push(element.data());
       });
+      this.products = product;
+      console.log(this.products);
     });
   }
 
