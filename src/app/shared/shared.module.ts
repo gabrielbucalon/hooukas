29e3741a3cab4075cce90eb86bcaae73fad36963
gabriel.bucalon <bucalongabriell@gmail.com>
2020-08-01 23:23:52 -0300

    feat: create component generic btn toggle to know the amount of snacks

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,20 +3,27 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MessageComponent } from './message/message.component';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ProductsService } from './services/products/products.service';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { CarouselComponent } from './carousel/carousel.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ButtonToggleComponent } from './button-toggle/button-toggle.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 
 
 @NgModule({
-  declarations: [CardComponent, MessageComponent, CarouselComponent],
+  declarations: [
+    CardComponent,
+    MessageComponent,
+    CarouselComponent,
+    ButtonToggleComponent
+  ],
   imports: [
     CommonModule,
     MatCardModule,
@@ -26,11 +33,17 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatFormFieldModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonToggleModule
   ],
   providers: [
     ProductsService
   ],
-  exports: [CardComponent, MessageComponent, CarouselComponent]
+  exports: [
+    CardComponent,
+    MessageComponent,
+    CarouselComponent,
+    ButtonToggleComponent
+  ]
 })
 export class SharedModule { }
