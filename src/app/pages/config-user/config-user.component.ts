0e4645812f92359311fba5e7a1ef208d9aae19c6
gabriel.bucalon <bucalongabriell@gmail.com>
2020-08-18 21:21:48 -0300

    feat: called user and component card

--- src/app/pages/config-user/config-user.component.ts
+++ src/app/pages/config-user/config-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../auth/model/User';
+import { Card } from '@/shared/card/model/Card';
 
 @Component({
   selector: 'app-config-user',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigUserComponent implements OnInit {
 
+  user: User;
+  card: Card;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem("currentUser"));
+    this.card = this.mountCard();
+  }
+
+  mountCard(): Card {
+    return {
+      title: `Configurações de ${this.user.name} =)`,
+      style: { backgroundColor: "#FFFFFF", margin: "1em" },
+    }
   }
 
 }
